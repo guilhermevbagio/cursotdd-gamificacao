548ad8adeff421f1d9035e4476558e179bac2383
Guilherme <guilhermevbagio@gmail.com>
2025-03-26 08:37:06 -0300

    quase pronto

--- src/main/java/com/cursotdd/Armazenamento.java
+++ src/main/java/com/cursotdd/Armazenamento.java
@@ -1,5 +1,56 @@
 package com.cursotdd;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Armazenamento {
 
+
+    public void salvar(Placar placar) throws IOException{
+            Path parentDir = Paths.get(System.getProperty("user.dir")).getParent();
+            
+            Path filePath = parentDir.resolve("savedFile.txt");
+
+
+            if (Files.notExists(filePath)) {
+                Files.createFile(filePath);
+            }
+
+            String content = "a";
+            Files.write(filePath, content.getBytes());
+    }
+
+
+    public String formatarPlacar(Placar placar){
+        for(Usuario usuario : placar.getUsuarios()){
+            
+
+            
+
+
+        }
+        
+        
+        
+        
+        
+        return "";
+    }
+
+
+
+    public String carregarDaPasta() throws IOException{
+        Path parentDir = Paths.get(System.getProperty("user.dir")).getParent();
+        Path filePath = parentDir.resolve("savedFile.txt");
+        return new String(Files.readAllBytes(filePath));
+    }
+
+    public Placar carregar() throws IOException{
+        String content = carregarDaPasta();
+
+        return new Placar();
+    }
 }
--- src/test/java/com/cursotdd/ArmazenamentoTest.java
+++ src/test/java/com/cursotdd/ArmazenamentoTest.java
@@ -1,5 +1,26 @@
 package com.cursotdd;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class ArmazenamentoTest {
-    
+    private Armazenamento armazenamento;
+
+    @BeforeEach
+    public void iniciar(){
+        armazenamento = new Armazenamento();
+    }
+
+    @Test
+    public void deveSalvarDados() throws IOException{
+        Placar placar = new Placar();
+        armazenamento.salvar(placar);
+    }
+
+    @Test
+    public void deveCarregarDados() throws IOException{
+        armazenamento.carregar();
+    }
 }
